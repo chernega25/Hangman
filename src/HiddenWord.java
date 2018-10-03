a309bcee77e770b08c8dd2cae2b5ed376d28c9de
chernega <chernega_nikita@mail.ru>
2018-10-03 22:34:02 +0300

    Add class HiddenWord with one option of word
    Hidden word is hangman

---
+++ src/HiddenWord.java
@@ -0,0 +1,37 @@
+public class HiddenWord {
+    private String word;
+    private boolean[] guessedLetters;
+    private int wordLength;
+
+    public hiddenWord() {
+        word = "hangman";
+        wordLength = word.length();
+        guessedLetters = new boolean[wordLength];
+    }
+
+    public boolean checkALetter(char letter) {
+        boolean guessed = false;
+
+        for (int i = 0; i < wordLength; i++) {
+            if (!guessedLetters[i] && word.charAt(i) == letter) {
+                guessedLetters[i] = true;
+                guessed = true;
+            }
+        }
+        return guessed;
+    }
+
+    public String getWordMask() {
+        StringBuilder wordMask = new StringBuilder();
+
+        for (int i = 0; i < wordLength; i++) {
+            if (guessedLetters[i]) {
+                wordMask = wordMask.append(word.charAt(i));
+            }
+            else {
+                wordMask = wordMask.append('*');
+            }
+        }
+        return wordMask.toString();
+    }
+}
